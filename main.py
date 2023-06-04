fd4258e904de957a2938aa552b9fe323b8a1daff
Grant Innes <129431946+nonProphet-90@users.noreply.github.com>
2023-06-05 00:26:37 +0300

    Initial comments & code layout

---
+++ main.py
@@ -0,0 +1,22 @@
+# represents card with suit & rank attributes
+class Card:
+
+    ...
+
+# rperesents deck, creates 52 card deck & shuffles. Include Method to deal cards to players
+class Deck:
+
+    ...
+
+# represents player in game, each player has name and list of cards. Include methods to play & add cards to & from stack
+class Player:
+
+    ...
+
+# controls main flow of the game with loops until winning conditions are met
+def main():
+
+    ...
+
+if __name__ == "__main__":
+    main()
