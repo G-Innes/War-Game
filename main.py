df6dba73083269cfe208fb8c096e8bd648589103
Grant Innes <129431946+nonProphet-90@users.noreply.github.com>
2023-06-06 12:33:41 +0300

    Added deal function & Player class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random 
 
-import random
 # represents card with suit & rank attributes
 class Card:
     def __init__(self, suit, rank):
@@ -20,22 +20,39 @@ class Deck:
             for rank in ranks:
                 self.cards.append(Card(suit, rank))
         random.shuffle(self.cards)
+        
+    # str overide to view deck is constructed as expected 
     def __str__(self):
         return str([str(card) for card in self.cards])
     
     # Include Method to deal cards to players
-
+    def deal(self):
+        if len(self.cards) > 0:
+            return self.cards.pop()
+        else:
+            return None
 
 # represents player in game, each player has name and list of cards. Include methods to play & add cards to & from stack
 class Player:
 
-    ...
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
 
 # controls main flow of the game with loops until winning conditions are met
 def main():
 
     deck = Deck()
     print(deck)
+
+    player1 = Player("Player 1")
+    player2 = Player("Player 2")
+
+    while len(deck.cards) > 0:
+        player1.cards.append(deck.deal())
+        player2.cards.append(deck.deal())
+    print(len(player1.cards))
+    print(len(player2.cards))
 
 if __name__ == "__main__":
     main()
